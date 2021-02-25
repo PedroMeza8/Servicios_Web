acc1ed7aacc77fadfacf9ba70b4def0a52667b39
PedroMeza8 <pedromeza8@hotmail.com>
2021-02-25 19:31:44 -0300

    Introduccion express

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-primer modificacion
+const express  = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('Mi respuesta desde Express')
+})
+
+app.get('/servicios', (req, res) => {
+    res.send('Estas en la pagina de servicios')
+})
+
+app.use((req, res, next) => {
+   res.status(404).sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
