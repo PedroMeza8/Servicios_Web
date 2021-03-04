640b37391e0d2eb03278696cd25b66ad14756c19
PedroMeza8 <pedromeza8@hotmail.com>
2021-03-04 19:56:52 -0300

    Utilizacion EJS

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const app = express();
 
 const port = 3000;
 
+// Motor de plantillas
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Mi respuesta desde Express')
+    res.render('Index', {titulo: 'Mi titulo dinamico'})
 })
 
 app.get('/servicios', (req, res) => {
-    res.send('Estas en la pagina de servicios')
+    res.render('servicios', {tituloServicios: 'Este es un mensaje dinamico de Servicios'})
 })
 
 app.use((req, res, next) => {
-   res.status(404).sendFile(__dirname + '/public/404.html')
+   res.status(404).render('404', {
+       titulo: '404',
+       descripcion: 'Titulo del sitio Web'
+   })
 })
 
 app.listen(port, () => {
