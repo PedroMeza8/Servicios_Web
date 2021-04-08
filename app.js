dc2cfe434b040b83c8d0fcb2373f36cfb23bb5c0
PedroMeza8 <pedromeza8@hotmail.com>
2021-04-08 19:50:32 -0300

    Creacion de Registros BD

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // const e = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 require('dotenv').config()
 
 const port = process.env.PORT || 3000;
--- router/Servicios.js
+++ router/Servicios.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -23,4 +24,22 @@ router.get('/',async (req,res)=>{
    
 })
 
+router.get('/crear', (req,res) => {
+    res.render('crear')
+})
+
+router.post('/', async (req,res) =>{
+    const body = req.body
+    try {
+      //  const ServicioDB = new Servicio(body) 
+      //  await ServicioDB.save()
+
+        await Servicio.create(body) 
+
+        res.redirect('/servicios')
+    } catch (error) {
+        console.log(error)
+    }
+}
+)
 module.exports = router;
